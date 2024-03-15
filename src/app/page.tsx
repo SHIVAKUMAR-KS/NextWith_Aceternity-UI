0b93fc0144f2a859c793a968aecb286b2cb528db
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-15 19:49:59 +0530

    Added featured card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import HeroSection from "@/components/ui/HeroSection";
+import FeaturesCourses from "@/components/FeaturesCourses";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-      <main className="min-h bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-            <HeroSection/>
+    <main className="min-h bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturesCourses/>
 
-      </main>    
+    </main>
   );
 }
