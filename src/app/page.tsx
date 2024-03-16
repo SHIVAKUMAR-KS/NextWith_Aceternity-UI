cfb9d9516d9e30ae010ec2d71af93000dc34e5b5
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-16 18:03:08 +0530

    Added whychooseus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturesCourses from "@/components/FeaturesCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/ui/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturesCourses/>
+      <WhyChooseUs/>
 
     </main>
   );
